4c40c6f7034cece7bbbfdbf8513b7aa6923c22df
Kiran Patel <kiran@inteleyes.com>
2022-11-12 13:36:51 +0000

    Made window open and close itself periodically. Also deals with user closing window manually.

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
 const { app, BrowserWindow } = require('electron')
 
+const period = .2*1/60 * 60 * 60 * 1000; // how often to create the window in ms for setInterval to use
+const viewingPeriod = .1 * 60 * 1000
+
 const createWindow = () => {
-  const win = new BrowserWindow({
+  let win = new BrowserWindow({
     show: false
+  })
+
+  win.on("close", () => {
+    win = null;
+    clearTimeout(closeTimeout)
   })
 
   win.loadFile('index.html')
   win.maximize()
+
+  // force window to close itself (timer) to make the user get started on some work and prevent them from putting win to the side and ignoring it.
+  // have to clear this timeout if window is closed by user.
+  const closeTimeout = setTimeout(() => {
+    win.close();
+    win = null;
+  }, viewingPeriod);
+
 }
+
+
 
 app.whenReady().then(() => {
   createWindow()
+  setInterval(createWindow, period)
 })
 
-app.setLoginItemSettings({
-    openAtLogin: true,
+// couldn't get the following to work, so the app will start itself every 2 hrs
+/*app.setLoginItemSettings({
+    openAtLogin: true
+})*/
 
-})
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+
+
+
+  // prank
+  // createWindow()
+  // createWindow()
+  // createWindow()
+
+  // obselete given 2 hr thing
+  // if (process.platform !== 'darwin') app.quit()
   // no need to keep it running - launch at login is fine
 })
